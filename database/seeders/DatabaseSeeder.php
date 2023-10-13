428cd0cbce099244c996bed4a621747c13507b72
Svetlana <sveta.sharymova@gmail.com>
2023-10-13 19:46:12 +0400

    43| feat:  delete seed news categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class,
-            NewsSeeder::class,
+//            CategorySeeder::class,
+//            NewsSeeder::class,
             UrlSeeder::class
         ]);
 
